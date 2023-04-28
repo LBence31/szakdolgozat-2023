1754f5773ff6c50790d806e08a3a1c43593891cd
Bence Lakosi <belga.1212@gmail.com>
2023-04-28 14:56:48 +0200

    refactor hooks/useSpotify

--- src/hooks/useSpotifyAuthCheck.ts
+++ src/hooks/useSpotifyAuthCheck.ts
@@ -1,7 +1,7 @@
 import { useSession, signIn } from "next-auth/react";
 import { useEffect } from "react";
 
-export default function spotifyErrorCheck() {
+export default function useSpotifyAuthCheck() {
   const { data: session } = useSession();
 
   useEffect(() => {
