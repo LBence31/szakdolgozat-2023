12cf1f1a356d748bd9cd4c32455f1e99f9693f4c
Bence Lakosi <belga.1212@gmail.com>
2023-04-28 14:31:25 +0200

    Added atoms

---
+++ src/pages/atoms/playlistAtom.ts
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const playlistIdState = atom({
+  key: "playlistIdState",
+  default: "5msih5DfnVPNG34KySmM5j",
+});
+
+export const playlistState = atom({
+  key: "playlistState",
+  default: null,
+});
