96e37607cc50aab77338b09cfe5ffeda9c6a50bf
Bence Lakosi <belga.1212@gmail.com>
2023-05-23 19:40:22 +0200

    Ts fix pt2

--- src/lib/time.ts
+++ src/lib/time.ts
@@ -1,7 +1,7 @@
 export function milisTiMinutesAndSecods(milis: number) {
   const minutes = Math.floor(milis / 60000);
   const seconds = ((milis % 60000) / 1000).toFixed(0);
-  return seconds == 60
+  return Number(seconds) == 60
     ? (minutes + 1).toString() + ":00"
-    : minutes.toString() + ":" + (seconds < 10 ? "0" : "") + seconds;
+    : minutes.toString() + ":" + (Number(seconds) < 10 ? "0" : "") + seconds;
 }
